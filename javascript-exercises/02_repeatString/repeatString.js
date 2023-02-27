7b7bbef69d209444e7f1cb090349bb69f827edd4
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-02-26 22:02:56 -0700

    Complete Exercise 2 in javascript-exercises

--- javascript-exercises/02_repeatString/repeatString.js
+++ javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let longstr = ""
+    for (let i = 0; i < num; i++) {
+        longstr += str
+    }
+    return longstr
 };
 
 // Do not edit below this line
