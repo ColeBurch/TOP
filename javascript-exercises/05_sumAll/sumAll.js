87aa62c13c911c18f7cc2af6d27df91c95d89899
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-02-28 16:48:24 -0700

    Complete exercise 5 in javascript exercises

--- javascript-exercises/05_sumAll/sumAll.js
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let largeNum = null
+    if (typeof num1 == 'number' && typeof num2 == 'number' && num1 > 0 && num2 > 0) {
+        if (num1 > num2) {
+            largeNum = num1
+        } else {
+            largeNum = num2
+        }
+        let sum = (num1 + num2) * (largeNum / 2)
+        return sum
+    } else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
