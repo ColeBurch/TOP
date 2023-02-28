a959749cc36986fc0da6d0c7bbe58452ca5c1658
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-02-28 16:27:01 -0700

    Complete exercise 4 in javascript-exercises

--- javascript-exercises/04_removeFromArray/removeFromArray.js
+++ javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    for (let i = 0; i < arguments[0].length; i++) {
+        for (let a = 0; a < arguments.length; a++) {
+            if (arguments[0][i] == arguments[a] && typeof arguments[0][i] == typeof arguments[a]) {
+                arguments[0].splice(i , 1)
+                i--
+            }
+        }
+    }
+    return arguments[0]
 };
 
 // Do not edit below this line
