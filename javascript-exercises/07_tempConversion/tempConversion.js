ebe340df5c171ac8b0af394289c85e4848129c51
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-03-01 10:43:36 -0700

    Complete exercise 7 in javascript exercises

--- javascript-exercises/07_tempConversion/tempConversion.js
+++ javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+    let cTemp = (fTemp - 32) * (5 / 9);
+    let roundCTemp = Math.round((cTemp + Number.EPSILON) * 10) / 10;
+    return roundCTemp
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+    let fTemp = (cTemp * (9 / 5)) + 32;
+    let roundFTemp = Math.round((fTemp + Number.EPSILON) * 10) / 10;
+    return roundFTemp
 };
 
 // Do not edit below this line
