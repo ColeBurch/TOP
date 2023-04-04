7ccba0d99830125fe58b19e782a93f005f8d13eb
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-04-04 17:14:22 -0600

    Have webpack create new index.html and clean all old files

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -4,9 +4,11 @@ import "./style.css";
 import Icon from "./coffee.svg";
 import Data from "./data.xml";
 import Notes from "./data.csv";
+import printMe from "./print.js";
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
 
   element.textContent = myName("Cody");
   element.classList.add("hello");
@@ -18,6 +20,11 @@ function component() {
 
   console.log(Data);
   console.log(Notes);
+
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
