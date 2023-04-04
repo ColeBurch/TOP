2717740291041dcd5573fbb1a855d0d9448267a8
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-04-04 16:44:45 -0600

    Handling an image with webpack

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,12 +1,18 @@
 import _ from "lodash";
 import myName from "./myName";
 import "./style.css";
+import Icon from "./coffee.svg";
 
 function component() {
   const element = document.createElement("div");
 
   element.textContent = myName("Cody");
   element.classList.add("hello");
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
